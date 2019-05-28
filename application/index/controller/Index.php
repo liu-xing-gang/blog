a6ef608c322c06ab59d851e089d9c61bcdd69eba
兴刚  刘 <xinggangliu@xinggangdeiMac.local>
2019-05-28 10:17:08 +0800

    create controller

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\index\controller;
+use think\Request;
+use think\Db;
 
 class Index
 {
@@ -7,5 +9,11 @@ class Index
     {
         // echo $_SERVER['REQUEST_SCHEME']."://".$_SERVER['HTTP_HOST'].'/static';
         return view('index');
+    }
+
+    public function test (){
+        $arr = array('a' => 1, 'b' => 2);
+        $data = Db::table('articles')->select();
+        return json_encode($data);
     }
 }
